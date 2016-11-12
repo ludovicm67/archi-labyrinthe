746c59524cdc35155fc5eabf96ab1c911a39aecb
ludovicm67 <ludovicmuller1@gmail.com>
2016-11-12 19:35:58 +0100

    Changement de condition : N doit être >=2

--- projet.s
+++ projet.s
@@ -1,6 +1,6 @@
 .data
-displayMenu: .asciiz "MENU :\n 1 - Génération d'un labyrinthe\n 2 - Résolution d'un labyrinthe\n\nEntrez votre choix : "
-Demande: .asciiz "Veuillez entrer un entier strictement supérieur à 0: "
+displayMenu: .asciiz "\nMENU :\n 1 - Génération d'un labyrinthe\n 2 - Résolution d'un labyrinthe\n\nEntrez votre choix : "
+Demande: .asciiz "Veuillez entrer un entier supérieur ou égal à 2 : "
 
 
 .text
@@ -22,30 +22,30 @@ li $t1 1
 li $t2 2
 beq $v0 $t1 genereLabyrinthe	# Choix 1 - Générer un labyrinthe
 beq $v0 $t2 resoudreLabyrinthe	# Choix 2 - Résoudre un labyrinthe
-j Menu				# Choix de l'utilisateur inexistant; on lui redemande
+j Menu				# Choix de l'utilisateur inexistant -> on lui redemande
 
 # Génération d'un labyrinthe
 genereLabyrinthe:
 
-j Affichage #Affichage de la demande à l'utilisateur
+j Affichage 			# Affichage de la demande à l'utilisateur
 
 Affichage: 
-la $a0 Demande #Chargement de la chaîne de caractère Demande dans $a0
-li $v0 4 #Affichage de la chaîne Demande
-syscall #Appel système
+la $a0 Demande 			# Chargement de la chaîne de caractère Demande dans $a0
+li $v0 4 			# Affichage de la chaîne Demande
+syscall 			# Appel système
 
-li $v0 5 #On lit l'entier que l'utilisateur a entré
+li $v0 5 			# On lit l'entier que l'utilisateur a entré
 syscall
 
 j Generation 
 
 Generation:
-move $a0 $v0 #On déplace la valeur que l'utilisateur a entré dans $v0
-li $v1, 0 #On attribye la valeur 0 à $v1
-ble $v0, $v1, Affichage #On test si $v0<=0 si c'est vrai on recommence à Affichage
-li $v0 1 #sinon on affiche $v0
+move $a0 $v0 			# On déplace la valeur que l'utilisateur a entré dans $v0
+li $v1, 2 			# On attribue la valeur 2 à $v1
+blt $v0, $v1, Affichage 	# On teste si $v0<2 si c'est vrai on recommence à Affichage
+li $v0 1 			# sinon on affiche $v0
 syscall
-j Exit #Le programme est finit
+j Exit				# Le programme est fini
 
 #Résolution d'un labyrinthe
 resoudreLabyrinthe:
