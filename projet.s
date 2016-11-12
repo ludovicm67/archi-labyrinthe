0787978375575bfc98d0c96107add29f33ca58d3
E-stein <eloise.stein@outlook.fr>
2016-11-12 14:18:42 +0100

    12/11/2016 14:18

--- projet.s
+++ projet.s
@@ -1,5 +1,6 @@
 .data
 displayMenu: .asciiz "MENU :\n 1 - Génération d'un labyrinthe\n 2 - Résolution d'un labyrinthe\n\nEntrez votre choix : "
+Demande: .asciiz "Veuillez entrer un entier strictement supérieur à 0: "
 
 
 .text
@@ -25,8 +26,26 @@ j Menu				# Choix de l'utilisateur inexistant; on lui redemande
 
 # Génération d'un labyrinthe
 genereLabyrinthe:
-## à compléter
-j Exit
+
+j Affichage #Affichage de la demande à l'utilisateur
+
+Affichage: 
+la $a0 Demande #Chargement de la chaîne de caractère Demande dans $a0
+li $v0 4 #Affichage de la chaîne Demande
+syscall #Appel système
+
+li $v0 5 #On lit l'entier que l'utilisateur a entré
+syscall
+
+j Generation 
+
+Generation:
+move $a0 $v0 #On déplace la valeur que l'utilisateur a entré dans $v0
+li $v1, 0 #On attribye la valeur 0 à $v1
+ble $v0, $v1, Affichage #On test si $v0<=0 si c'est vrai on recommence à Affichage
+li $v0 1 #sinon on affiche $v0
+syscall
+j Exit #Le programme est finit
 
 #Résolution d'un labyrinthe
 resoudreLabyrinthe:
@@ -35,5 +54,5 @@ j Exit
 
 # Fin du programme
 Exit:
-li $v0 10
+li $v0 10 
 syscall
