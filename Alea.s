2a4008a2c96c6c9eeb4d9c83abf832b95de72f52
ludovicm67 <ludovicmuller1@gmail.com>
2016-11-16 14:26:24 +0100

    add some files + edit project

---
+++ Alea.s
@@ -0,0 +1,91 @@
+#           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                   Version 2, December 2004
+#
+# Copyright (C) 2016 Maxime Schmitt <max.schmitt@math.unistra.fr>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+# 0. You just DO WHAT THE FUCK YOU WANT TO.
+
+.data
+seed: .word 0xfaceb00c, 0xdeadbeef
+max_float_alea: .float 2147483647
+
+.text
+.globl __start
+
+# Point d'entrée du programme
+__start:
+li $a0, 0
+li $a1, 101
+jal nombre_alea_entre_deux_bornes
+move $a0, $v0
+li $v0, 1
+syscall
+j exit
+
+# Retourne un nombre aléatoire inclus dans l'intervalle [$a0,$a1[
+# Paramètres : a passé dans $a0 et b passé dans $a1
+# Pré-conditions : 0 <= $a0 < $a1
+# Résultat : le nombre aléatoire dans le registre $v0
+nombre_alea_entre_deux_bornes:
+# Prologue
+subu $sp, $sp, 12
+sw $ra, ($sp)
+swc1 $f0, 4($sp)
+swc1 $f1, 8($sp)
+# Corps
+jal random_generator
+andi $v0, $v0, 0x7fffffff
+mtc1 $v0, $f0
+cvt.s.w $f0, $f0
+lwc1 $f1, max_float_alea
+div.s $f0, $f0, $f1
+sub $a1, $a1, $a0
+mtc1 $a1, $f1
+cvt.s.w $f1, $f1
+mul.s $f0, $f0, $f1
+mtc1 $a0, $f1
+cvt.s.w $f1, $f1
+add.s $f0, $f0, $f1
+cvt.w.s $f0, $f0
+mfc1 $v0, $f0
+# Épilogue
+lw $ra, ($sp)
+lwc1 $f0, 4($sp)
+lwc1 $f1, 8($sp)
+addu $sp, $sp, 12
+jr $ra
+
+# Function random_generator
+# Retourne un nombre aléatoire entre 0 et 2^32-1
+# Paramètres :
+# Résultat : Le nombre aléatoire dans $v0
+random_generator:
+# Prologue
+# Corps
+lw $t0, seed
+andi $t1, $t0, 65535
+mulu $t1, $t1, 36969
+srl $t2, $t0, 16
+addu $t0, $t1, $t2
+lw $t1, seed+4
+andi $t2, $t1, 65535
+mulu $t2, $t2, 18000
+srl $t3, $t1, 16
+addu $t1 $t2, $t3
+sw $t0, seed
+sw $t1, seed+4
+sll $v0, $t0, 16
+addu $v0, $v0, $t1
+# Épilogue
+jr $ra
+
+exit:
+li $v0, 10
+syscall
--- L-projet.s
+++ L-projet.s
@@ -18,8 +18,8 @@ li $v0 4			# On dit que l'on souhaite afficher une chaîne de caractère
 syscall				# On effectue l'appel système
 li $v0 5			# On demande à l'utilisateur de saisir un entier (sera stocké dans $v0)
 syscall				# On effectue l'appel système
-li $t1 1
-li $t2 2
+li $t1 1			# On met t1 = 1
+li $t2 2			# On met t2 = 2
 beq $v0 $t1 genereLabyrinthe	# Choix 1 - Générer un labyrinthe
 beq $v0 $t2 resoudreLabyrinthe	# Choix 2 - Résoudre un labyrinthe
 j Menu				# Choix de l'utilisateur inexistant -> on lui redemande
@@ -27,19 +27,13 @@ j Menu				# Choix de l'utilisateur inexistant -> on lui redemande
 # Génération d'un labyrinthe
 genereLabyrinthe:
 
-j Affichage 			# Affichage de la demande à l'utilisateur
-
+# Affichage de la demande à l'utilisateur
 Affichage: 
 la $a0 Demande 			# Chargement de la chaîne de caractère Demande dans $a0
 li $v0 4 			# Affichage de la chaîne Demande
 syscall 			# Appel système
-
 li $v0 5 			# On lit l'entier que l'utilisateur a entré
 syscall
-
-j Generation 
-
-Generation:
 move $a0 $v0 			# On déplace la valeur que l'utilisateur a entré dans $v0
 li $v1, 2 			# On attribue la valeur 2 à $v1
 blt $v0, $v1, Affichage 	# On teste si $v0<2 si c'est vrai on recommence à Affichage
