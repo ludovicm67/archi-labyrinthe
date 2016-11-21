ea4fdd7c4f468f5f383c575237e06dc77b4e6895
E-stein <eloise.stein@outlook.fr>
2016-11-21 16:03:43 +0100

    <3

--- E-projet.s
+++ E-projet.s
@@ -304,81 +304,42 @@ MenucaseDetF:
 	sw $ra 0($sp)
 	
 	#corps de la fonction
-	li $s0 0 #choix 0: D à gauche, F à droite
-	li $s1 1 #choix 1: D à droite, F à gauche
-	li $s2 2 #choix 2: D en haut, F en bas
-	li $s3 3 #choix 3: D en bas, F en haut
-	li $a0 0
-	li $a1 4
-	jal nombre_alea_entre_deux_bornes
-	move $a0 $v0
+	li $s0 0 #D à gauche, F à droite
+	li $s1 1 #D à droite, F à gauche
+	li $s2 2 #D en haut, F en bas
+	li $s3 3 #D en bas, F en haut
+	li $a0 0 #borne 1
+	li $a1 4 #borne 2
+	li $v0 42 #genere un nombre aléatoire $a0 , borne 1<=$a0 < borne 2
+	syscall
+	move $t1 $a0 # On déplace le nombre aleatoire dans $t1
+	lw $a1 16($sp) #On stocke dans $a1 la valeur initiale de $a0: N
+	move $a0 $s0 #On stocke dans $a0 la valeur de $s0 :0
+	li $v0 42 #On genere un nouveau nombre aleatoire: $a0<= $a0 <$a1
+	syscall
 	li $v0 1
 	syscall
+	move $t2 $a0 #On stocke ce nouveau nombre dans $t2
+	li $v0 42 #On genere un nouveau nombre aleatoire: $a0<= $a0 <$a1
+	syscall
+	li $v0 1 
+	syscall
+	move $t3 $a0 #On stocke ce nouveau nombre dans $t3
+	#beq $t1 $s0 cas0 #si le nombre aleatoire vaut 0 alors D à gauche, F à droite
+	#beq $t1 $s1 cas1 #si le nombre aleatoire vaut 0 alors D à droite, F à gauche
+	#beq $t1 $s2 cas2 #si le nombre aleatoire vaut 0 alors D en haut, F en bas
+	#beq $t1 $s3 cas3 #si le nombre aleatoire vaut 0 alors D en bas, F en haut
+	
+	#cas0:
+	
 	
 	#epilogue
-	lw $a0 16($sp)
+	lw $a0 1	²6($sp)
 	lw $a1 12($sp)
 	lw $a2 8($sp)
 	lw $a3 4($sp)
 	lw $ra 0($sp)
 	addu $sp $sp 20
-	jr $ra
-	
-# Retourne un nombre aléatoire inclus dans l'intervalle [$a0,$a1[
-# Paramètres : a passé dans $a0 et b passé dans $a1
-# Pré-conditions : 0 <= $a0 < $a1
-# Résultat : le nombre aléatoire dans le registre $v0
-nombre_alea_entre_deux_bornes:
-	# Prologue
-	subu $sp, $sp, 12
-	sw $ra, ($sp)
-	swc1 $f0, 4($sp)
-	swc1 $f1, 8($sp)
-	# Corps
-	jal random_generator
-	andi $v0, $v0, 0x7fffffff
-	mtc1 $v0, $f0
-	cvt.s.w $f0, $f0
-	lwc1 $f1, max_float_alea
-	div.s $f0, $f0, $f1
-	sub $a1, $a1, $a0
-	mtc1 $a1, $f1
-	cvt.s.w $f1, $f1
-	mul.s $f0, $f0, $f1
-	mtc1 $a0, $f1
-	cvt.s.w $f1, $f1
-	add.s $f0, $f0, $f1
-	cvt.w.s $f0, $f0
-	mfc1 $v0, $f0
-	# Épilogue
-	lw $ra, ($sp)
-	lwc1 $f0, 4($sp)
-	lwc1 $f1, 8($sp)
-	addu $sp, $sp, 12
-	jr $ra
-
-# Function random_generator
-# Retourne un nombre aléatoire entre 0 et 2^32-1
-# Paramètres :
-# Résultat : Le nombre aléatoire dans $v0
-random_generator:
-	# Prologue
-	# Corps
-	lw $t0, seed
-	andi $t1, $t0, 65535
-	mulu $t1, $t1, 36969
-	srl $t2, $t0, 16
-	addu $t0, $t1, $t2
-	lw $t1, seed+4
-	andi $t2, $t1, 65535
-	mulu $t2, $t2, 18000
-	srl $t3, $t1, 16
-	addu $t1 $t2, $t3
-	sw $t0, seed
-	sw $t1, seed+4
-	sll $v0, $t0, 16
-	addu $v0, $v0, $t1
-	# Épilogue
 	jr $ra
 	
 #Résolution d'un labyrinthe
